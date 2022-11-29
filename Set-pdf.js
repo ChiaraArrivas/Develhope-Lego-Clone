3698b886b56fde3a19b06f2c92a2bc6806ea096f
Salvatore Vella <salvatorevella1234@gmail.com>
2022-11-29 18:20:27 +0100

    Carousel working

---
+++ Set-pdf.js
@@ -0,0 +1,41 @@
+const prevBtn = document.querySelector(".prev-btn")
+const nextBtn = document.querySelector(".next-btn")
+const carouselBody = document.querySelector(".carousel-body")
+
+function CarouselNoRestart(previousButton, nextButton, carouselBody) {
+    carouselBody.style.scrollBehavior = "smooth";
+    prevBtn.style.visibility = "hidden";
+
+    previousButton.addEventListener("click", () => {
+        let scrollStep = carouselBody.clientWidth;
+        carouselBody.scrollLeft -= scrollStep;
+    })
+
+    nextButton.addEventListener("click", () => {
+        let scrollStep = carouselBody.clientWidth;
+        carouselBody.scrollLeft += scrollStep;
+    })
+
+    carouselBody.addEventListener("scroll", () => {
+        let scrollStep = carouselBody.clientWidth;
+        let scrollLength = carouselBody.scrollWidth - scrollStep;
+        if (Math.ceil(carouselBody.scrollLeft) == scrollLength) {
+            nextBtn.style.visibility = "hidden";
+        } else {
+            nextBtn.style.visibility = "visible"
+        }
+        if (Math.floor(carouselBody.scrollLeft) == scrollLength) {
+            nextBtn.style.visibility = "hidden";
+        } else {
+            nextBtn.style.visibility = "visible"
+        }
+        if (Math.floor(carouselBody.scrollLeft) == 0) {
+            prevBtn.style.visibility = "hidden";
+        } else {
+            prevBtn.style.visibility = "visible"
+        }
+        console.log(scrollLength, carouselBody.scrollLeft);
+    })
+}
+
+CarouselNoRestart(prevBtn, nextBtn, carouselBody);
