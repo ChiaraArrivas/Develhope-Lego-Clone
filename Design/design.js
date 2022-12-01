fe3315bc436a1b5d60b6ea4a0b53ec5fba21a1a0
Salvatore Vella <salvatorevella1234@gmail.com>
2022-12-01 13:32:45 +0100

    Feat: Carousel consigliati per te

---
+++ Design/design.js
@@ -0,0 +1,30 @@
+export function CarouselNoRestart(previousButton, nextButton, carouselBody) {
+    carouselBody.style.scrollBehavior = "smooth";
+    previousButton.style.visibility = "hidden";
+
+    previousButton.addEventListener("click", () => {
+        let scrollStep = carouselBody.clientWidth;
+        carouselBody.scrollLeft -= scrollStep;
+    })
+
+    nextButton.addEventListener("click", () => {
+        let scrollStep = carouselBody.clientWidth;
+        carouselBody.scrollLeft += scrollStep;
+    })
+
+    carouselBody.addEventListener("scroll", () => {
+        let scrollStep = carouselBody.clientWidth;
+        let scrollLength = carouselBody.scrollWidth - scrollStep;
+        if (Math.ceil(carouselBody.scrollLeft) == scrollLength || Math.floor(carouselBody.scrollLeft) == scrollLength) {
+            nextButton.style.visibility = "hidden";
+        } else {
+            nextButton.style.visibility = "visible"
+        }
+        if (Math.floor(carouselBody.scrollLeft) == 0) {
+            previousButton.style.visibility = "hidden";
+        } else {
+            previousButton.style.visibility = "visible"
+        }
+    })
+}
+
